f93bd128ac5c19c1d33b82808ee38e0cfda0535b
demn <demndevel@gmail.com>
2024-09-01 00:54:10 +0300

    fix: add translation for google login btn

--- feature/login/src/main/java/me/floow/login/ui/components/GoogleLoginButton.kt
+++ feature/login/src/main/java/me/floow/login/ui/components/GoogleLoginButton.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import me.floow.uikit.util.ComponentPreviewBox
@@ -34,7 +35,7 @@ internal fun GoogleLoginButton(
         Spacer(Modifier.width(8.dp))
 
         Text(
-            text = "Login",
+            text = stringResource(R.string.google_login_btn_text),
             color = MaterialTheme.colorScheme.primary,
             style = MaterialTheme.typography.titleMedium,
         )

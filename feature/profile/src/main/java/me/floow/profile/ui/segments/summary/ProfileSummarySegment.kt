266ae3ccf75c8b620f8c807f21986c20e04fcfa5
demn <demndevel@gmail.com>
2024-10-15 21:45:11 +0300

    feat(login): implement horizontal pager indicator

--- feature/profile/src/main/java/me/floow/profile/ui/segments/summary/ProfileSummarySegment.kt
+++ feature/profile/src/main/java/me/floow/profile/ui/segments/summary/ProfileSummarySegment.kt
@@ -2,13 +2,22 @@ package me.floow.profile.ui.segments.summary
 
 import android.net.Uri
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import me.floow.profile.uilogic.ProfileSubscribers
@@ -21,35 +30,67 @@ fun ProfileSummarySegment(
 	subscribers: ProfileSubscribers,
 	modifier: Modifier = Modifier
 ) {
-	val pagerState = rememberPagerState(initialPage = 0, pageCount = { 2 })
+	val pageCount = 2
+	val pagerState = rememberPagerState(initialPage = 0, pageCount = { pageCount })
 
-	HorizontalPager(
-		state = pagerState,
-		modifier = modifier
-			.height(330.dp)
-	) { page ->
-		Box(
-			modifier = modifier
-				.background(Color.DarkGray)
-				.fillMaxSize()
-		) {
-			when (page) {
-				0 -> {
-					AvatarUsernameProfileSummaryPage(
-						profileAvatarUri = profileAvatarUri,
-						displayName = displayName,
-						subscribers = subscribers,
-						modifier = Modifier.fillMaxSize()
-					)
-				}
+	Box(modifier) {
+		HorizontalPager(
+			state = pagerState,
+			modifier = Modifier
+				.height(330.dp)
+		) { page ->
+			Box(
+				modifier = Modifier
+					.background(Color.DarkGray)
+					.fillMaxSize()
+			) {
+				when (page) {
+					0 -> {
+						AvatarUsernameProfileSummaryPage(
+							profileAvatarUri = profileAvatarUri,
+							displayName = displayName,
+							subscribers = subscribers,
+							modifier = Modifier.fillMaxSize()
+						)
+					}
 
-				1 -> {
-					AboutMeProfileSummaryPage(
-						description = description,
-						modifier = Modifier.fillMaxSize()
-					)
+					1 -> {
+						AboutMeProfileSummaryPage(
+							description = description,
+							modifier = Modifier.fillMaxSize()
+						)
+					}
 				}
 			}
+		}
+
+		PagerIndicator(
+			pageCount = pageCount,
+			currentPage = pagerState.currentPage,
+			modifier = Modifier
+				.align(Alignment.BottomCenter)
+				.padding(bottom = 22.dp)
+		)
+	}
+}
+
+@Composable
+private fun PagerIndicator(
+	currentPage: Int,
+	pageCount: Int,
+	modifier: Modifier = Modifier
+) {
+	Row(
+		modifier = modifier,
+		horizontalArrangement = Arrangement.spacedBy(4.dp)
+	) {
+		for (page in 0..<pageCount) {
+			Box(
+				modifier = Modifier
+					.clip(CircleShape)
+					.background(if (currentPage == page) Color.White else Color.LightGray)
+					.size(6.dp)
+			)
 		}
 	}
 }

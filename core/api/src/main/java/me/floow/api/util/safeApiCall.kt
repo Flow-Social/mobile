355be694869d5efcca607689ffbad86790d72ba9
demn <demndevel@gmail.com>
2024-11-01 21:20:45 +0300

    feat(api): handle internet interruption cases for safeApiCall

--- core/api/src/main/java/me/floow/api/util/safeApiCall.kt
+++ core/api/src/main/java/me/floow/api/util/safeApiCall.kt
@@ -1,6 +1,8 @@
 package me.floow.api.util
 
 import io.ktor.util.network.*
+import io.ktor.utils.io.core.EOFException
+import io.ktor.utils.io.errors.IOException
 import kotlinx.coroutines.TimeoutCancellationException
 
 suspend fun <T> safeApiCall(errorResponse: T, apiCall: suspend () -> T): T {
@@ -9,6 +11,10 @@ suspend fun <T> safeApiCall(errorResponse: T, apiCall: suspend () -> T): T {
 	} catch (ex: UnresolvedAddressException) {
 		errorResponse
 	} catch (ex: TimeoutCancellationException) {
+		errorResponse
+	} catch (ex: EOFException) {
+		errorResponse
+	} catch (ex: IOException) {
 		errorResponse
 	}
 }
